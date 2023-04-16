78b6e318ee0b1948af4ea77b4e237055e077bfd0
Ashmin <dbswoals0512@gmail.com>
2023-04-16 17:25:22 +0900

    [백준17086] 아기 상어 2 - 수정 속도 향상

--- 17086.cpp
+++ 17086.cpp
@@ -3,13 +3,14 @@
 #include <algorithm>
 using namespace std;
 const int MAX_SIZE = 50;
+const int INF = 987654321;
 
-int is_shark[MAX_SIZE][MAX_SIZE];
+bool is_shark[MAX_SIZE][MAX_SIZE];
+int dist[MAX_SIZE][MAX_SIZE];
 
 int N, M;
 int dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
 int dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-bool visited[MAX_SIZE][MAX_SIZE];
 
 bool OutOfRange(int y, int x) {
 	if (y < 0 || y >= N) return true;
@@ -32,14 +33,18 @@ void Input() {
 	}
 }
 
-int BfsFindSafetyDistance(int y, int x) {
+void BfsWriteDistance(int y, int x) {
+	if (!is_shark[y][x])
+		return;
 
+	// 상어 기준 거리를 작성
 	queue<pair<int, int> > q;
 	queue<int> cnt_q;
 	q.push(make_pair(y, x));
 	cnt_q.push(0);
 
 	int cnt = 0;
+	dist[y][x] = 0; // 상어 자기 자신
 
 	while(!q.empty())
 	{
@@ -49,52 +54,68 @@ int BfsFindSafetyDistance(int y, int x) {
 		q.pop();
 		cnt_q.pop();
 
-		if (is_shark[y][x])
-			return cnt;
-		visited[y][x] = true;
 
 		// 이동
 		for (int i = 0; i < 8; i++) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
+			int ncnt = cnt + 1;
 
 			if (OutOfRange(ny, nx)) continue;
-			if (visited[ny][nx]) continue;
+			// 안전 거리가 더 짧다면
+			// 더 진행할 필요 없음
+			if (dist[ny][nx] <= ncnt) continue; 
 
-			visited[ny][nx] = true;
+			// 안전 거리 갱신
+			dist[ny][nx] = ncnt;
 			q.push(make_pair(ny, nx));
-			cnt_q.push(cnt + 1);
+			cnt_q.push(ncnt);
 		}
-
 	}
 
-	return cnt;
 }
 
-void InitVisited() {
+void InitDist() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			visited[i][j] = false;
+			dist[i][j] = INF;
 		}
+	}
+}
+
+void PrintDist() {
+	cout << "PrintDist() ===========" << endl;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << dist[i][j] << " ";
+		}
+		cout << endl;
 	}
 }
 
 int Solution() {
 	int max_dist = 0;
+	InitDist(); // 거리 초기화
 
+	// 전체 상어 안전 거리 작성
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			// 상어가 아닐 때
-			if (!is_shark[i][j])
-			{
-				InitVisited();
-				max_dist = max(max_dist, BfsFindSafetyDistance(i, j));
-			}
+			BfsWriteDistance(i, j);
+		}
+	}
+
+	// PrintDist();
+
+	// 최대 안전 거리 찾기
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			max_dist = max(max_dist, dist[i][j]);
 		}
 	}
 
 	return max_dist;
 }
+
 
 int main() {
 
