949f5c56be199f77bda51353ca9ab26c0c67c025
Ashmin <dbswoals0512@gmail.com>
2023-05-10 17:45:57 +0900

    [백준5582] 공통 부분 문자열 - 성능향상(다른 사람 코드 참고)

--- 5582.cpp
+++ 5582.cpp
@@ -2,13 +2,18 @@
 #include <algorithm>
 
 using namespace std;
-const int MAX_SIZE = 4000;
+const int MAX_SIZE = 4001;
 
 string str1;
 string str2;
 int ans = 0;
 
 int common_len[MAX_SIZE][MAX_SIZE];
+
+// 내 풀이가 느렸던 원인
+// dp 를 뒤쪽 방향으로 뒀는데
+// 앞에서부터 진행해서 bottom up으로 해야함
+// https://www.acmicpc.net/source/59573266
 
 void Init() {
 	for (int i = 0; i < MAX_SIZE; i++) {
@@ -19,6 +24,7 @@ void Init() {
 }
 
 
+/*
 int GetCommonLength(int i, int j) {
 	if (i == str1.size() || j == str2.size()) {
 		return 0;
@@ -48,6 +54,7 @@ int GetCommonLength(int i, int j) {
 	ans = max(ans, ret); // 최대값 갱신
 	return ret;
 }
+*/
 
 
 
@@ -55,12 +62,27 @@ int main() {
 	cin >> str1;
 	cin >> str2;
 
-	Init();
 
+	//Init();
 	// str1 순회
+	/*
 	for (int i = 0; i < str1.size(); i++) {
 		GetCommonLength(i, 0);
 	}
+	*/
+
+
+	// 1부터 시작
+	// i - 1 최소 값이 0 되도록
+	for (int i = 1; i <= str1.size(); i++) {
+		for (int j = 1; j <= str2.size(); j++) {
+			if (str1[i - 1] == str2[j - 1]) {
+				common_len[i][j] = common_len[i - 1][j - 1] + 1;
+				ans = max(ans, common_len[i][j]);
+			}
+		}
+	}
+
 	cout << ans;
 
 	return 0;
